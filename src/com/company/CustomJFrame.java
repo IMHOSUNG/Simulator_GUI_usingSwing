885dfd445fcae645d8d783158d7e4f750711eabb
IMHOSUNG <dlaghtjd4487@dongguk.edu>
2018-09-11 21:01:23 +0900

    쓰레드처리끝,위치이동끝

--- src/com/company/CustomJFrame.java
+++ src/com/company/CustomJFrame.java
@@ -5,19 +5,36 @@ import java.awt.*;
 
 public class CustomJFrame extends JFrame {
 
-
+    Custompanel panel1 = null;
+    Custompanel panel2 = null;
+    Custompanel panel3 = null;
+    JPanel bottom = new JPanel();
 
     CustomJFrame(int id1, int id2, int id3)
     {
-        Custompanel panel1 = new Custompanel(new twoIntValue(1000,10,0),id1);
-        Custompanel panel2 = new Custompanel(new twoIntValue(1000,10,0),id2);
-        Custompanel panel3 = new Custompanel(new twoIntValue(1000,10,0),id3);
 
-        setLayout(new BorderLayout());
-        setVisible(true);
+        setLayout(null);
+        panel1 = new Custompanel(new twoIntValue(1000,10,0),new twoIntValue(0,0,1),id1);
+        panel2 = new Custompanel(new twoIntValue(1000,10,0),new twoIntValue(100,100,1),id2);
+        panel3 = new Custompanel(new twoIntValue(1000,10,0),new twoIntValue(200,200,1),id3);
+
+
+        //panel1.setPreferredSize(new Dimension(400,400));
+        //panel1.setLayout(null);
+        panel1.setSize(200,200);
+        panel1.setLocation(100,100);
+        getContentPane().add(panel1);
+
+        panel2.setSize(200,200);
+        panel2.setLocation(300,300);
+        getContentPane().add(panel2);
+
+        panel3.setSize(200,200);
+        panel3.setLocation(0,0);
+        getContentPane().add(panel3);
+
         setSize(500,500);
-        add(panel1);
-        add(panel2);
-        add(panel3);
+        setVisible(true);
+
     }
 }
--- src/com/company/Custompanel.java
+++ src/com/company/Custompanel.java
@@ -3,6 +3,7 @@ package com.company;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -17,7 +18,7 @@ class Custompanel extends JPanel {
     private JPanel window = new JPanel();
 
 
-    public Custompanel(twoIntValue in, int id )
+    public Custompanel(twoIntValue in, twoIntValue pos , int id )
     {
         setLayout(null);
         this.id = id;
@@ -38,41 +39,12 @@ class Custompanel extends JPanel {
         valueArea.setText(String.valueOf(value));
         add(valueArea);
 
+        setSize(new Dimension(120,100));
         upButton.addActionListener(new upButtonListener());
         downButton.addActionListener(new downButtonListener());
-
         Runnable runnable = new TextAreaThread(valueArea,in,value, id);
         Thread thread = new Thread(runnable);
         thread.start();
-    }
-
-    public Custompanel(twoIntValue upButton_Location, twoIntValue downButton_Location, twoIntValue valueArea_Location, twoIntValue in)
-    {
-        setLayout(null);
-
-        upButton = new JButton("up");
-        upButton.setSize(70,20);
-        upButton.setLocation(upButton_Location.x , upButton_Location.y);
-        add(upButton);
-
-        valueArea = new JTextArea();
-        valueArea.setLocation(valueArea_Location.x, valueArea_Location.y);
-
-        downButton = new JButton("down");
-        downButton.setSize(70,20);
-        downButton.setLocation(downButton_Location.x, downButton_Location.y);
-
-        upButton.addActionListener(new upButtonListener());
-        downButton.addActionListener(new downButtonListener());
-
-        Runnable runnable = new TextAreaThread(valueArea,in,value, id);
-        Thread thread = new Thread(runnable);
-        thread.start();
-    }
-
-    void setValue(int random_number)
-    {
-        this.value = random_number;
     }
 
     class upButtonListener implements ActionListener {
